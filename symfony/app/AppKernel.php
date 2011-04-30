db4c370c08671a5cb079288b292f62599581482d
Torgi <torgipf@gmail.com>
2011-04-30 16:34:36 +0400

    [Users] installation

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\DemoBundle\AcmeDemoBundle(),
+            new Bundle\Users\BundleUsers(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
